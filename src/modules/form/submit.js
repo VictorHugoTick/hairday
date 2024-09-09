537ddedd55e71705d5796498cb5892e189b60357
Victor Assir <victor.assir@gmail.com>
2024-09-09 16:05:14 -0300

    updating javascript

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,9 @@
 import dayjs from "dayjs"
 
+import { scheduleNew } from "../../services/schedule-new.js"
+
 const form = document.querySelector("form")
+const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
 
 // Data atual para formatar o input
@@ -10,7 +13,40 @@ const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
 selectedDate.value = inputToday
 selectedDate.min = inputToday
 
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
   // Previne o comportamento padrão de carregar a página
   event.preventDefault()
+
+  try {
+    // Recuperando o nome do cliente.
+    const name = clientName.value.trim()
+
+    if(!name) {
+      return alert("Informe o nome do cliente!")
+    }
+
+    // Recupera o horário selecionado.
+    const hourSelected = document.querySelector(".hour-selected")
+    
+    if (!hourSelected) {
+      return alert("Selecione a hora.")
+    }
+
+    // Recupera somente a hora.
+    const [hour] = hourSelected.innerText.split(":")
+
+    // Insere a hora na data.
+    const when = dayjs(selectedDate.value).add(hour, "hour")
+
+    // Gera um id.
+    const id = new Date().getTime()
+
+    await scheduleNew({
+      id, name, when
+    })
+    
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento.")
+    console.log(error)
+  }
 }
